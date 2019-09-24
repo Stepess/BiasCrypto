01d76253456ef6d969667e9326c7233341e2e624
Stepess <stepanersh@gmail.com>
2019-09-24 22:19:48 +0300

    distro calculated

--- src/main/java/com/kpi/ipt/crypto/Main.java
+++ src/main/java/com/kpi/ipt/crypto/Main.java
@@ -28,30 +28,15 @@ public class Main {
 
         int[][] cipherMatrix = ArrayMapper.mapToMatrix(csvReader.read("table_02.csv"));
 
-        double[] ciphertextDistribution = new double[CIPHERTEXT_POWER];
+        double[] ciphertextDistribution =
+                calculateCipherDistribution(messageDistribution, keyDistribution, cipherMatrix);
 
-
-        /*for (int cipher = 0; cipher < CIPHERTEXT_POWER; cipher++) {
-            for (int key = 0; key < CIPHERTEXT_POWER; key++) {
-                for (int message = 0; message < CIPHERTEXT_POWER; message++) {
-                    if (cipherMatrix[key][message] == cipher) {
-                        ciphertextDistribution[cipher] += messageDistribution[message] * keyDistribution[key];
-                    }
-                }
-            }
-        }*/
-
-        for (int i = 0; i < cipherMatrix.length; i++) {
+        /*for (int i = 0; i < cipherMatrix.length; i++) {
             for (int j = 0; j < cipherMatrix[i].length; j++) {
                 int ciphertextNumber = cipherMatrix[i][j];
                 ciphertextDistribution[ciphertextNumber] += messageDistribution[j] * keyDistribution[i];
             }
-        }
-
-        double[][] messageAndCiphertextDistribution = new double[CIPHERTEXT_POWER][CIPHERTEXT_POWER];
-        for (int i = 0; i < CIPHERTEXT_POWER; i++) {
-            messageAndCiphertextDistribution[i] = new double[CIPHERTEXT_POWER];
-        }
+        }*/
 
         /*for (int cipher = 0; cipher < CIPHERTEXT_POWER; cipher++) {
             for (int message = 0; message < CIPHERTEXT_POWER; message++) {
@@ -61,26 +46,77 @@ public class Main {
             }
         }*/
 
-        for (int i = 0; i < messageAndCiphertextDistribution.length; i++) {
-            for (int j = 0; j < messageAndCiphertextDistribution[i].length; j++) {
-                for (int cipher = 0; cipher < CIPHERTEXT_POWER; cipher++) {
-                    if (cipher == cipherMatrix[i][j]) {
-                        messageAndCiphertextDistribution[j][i] += messageDistribution[j] * keyDistribution[i];
-                    }
-                }
-            }
-        }
+        double[][] messageAndCiphertextDistribution =
+                calculateMessageAndCipherDistribution(messageDistribution, keyDistribution, cipherMatrix);
 
+
+        double[][] messageConditionalByCipherDistribution =
+                calculateMessageConditionalByCipherDistribution(ciphertextDistribution, messageAndCiphertextDistribution);
+
+    }
+
+    private static double[][] calculateMessageConditionalByCipherDistribution(double[] ciphertextDistribution, double[][] messageAndCiphertextDistribution) {
         double[][] messageConditionalCiphertextDistribution = new double[CIPHERTEXT_POWER][CIPHERTEXT_POWER];
         for (int i = 0; i < CIPHERTEXT_POWER; i++) {
             messageConditionalCiphertextDistribution[i] = new double[CIPHERTEXT_POWER];
         }
 
-        for (int i = 0; i < CIPHERTEXT_POWER; i++) {
-            for (int j = 0; j < CIPHERTEXT_POWER; j++) {
-                messageConditionalCiphertextDistribution[i][j] = messageAndCiphertextDistribution[i][j] / ciphertextDistribution[j];
+
+        for (int message = 0; message < messageAndCiphertextDistribution.length; message++) {
+            for (int cipher = 0; cipher < CIPHERTEXT_POWER; cipher++) {
+                messageConditionalCiphertextDistribution[cipher][message] =
+                        messageAndCiphertextDistribution[cipher][message] /
+                                ciphertextDistribution[cipher];
             }
         }
 
+        return messageConditionalCiphertextDistribution;
     }
+
+    private static double[] calculateCipherDistribution(
+            double[] messageDistribution,
+            double[] keyDistribution,
+            int[][] cipherMatrix) {
+        double[] ciphertextDistribution = new double[CIPHERTEXT_POWER];
+
+        for (int cipher = 0; cipher < CIPHERTEXT_POWER; cipher++) {
+
+            for (int key = 0; key < keyDistribution.length; key++) {
+                for (int message = 0; message < messageDistribution.length; message++) {
+                    if (cipherMatrix[key][message] == cipher) {
+                        ciphertextDistribution[cipher] += messageDistribution[message] * keyDistribution[key];
+                    }
+                }
+            }
+
+        }
+
+        return ciphertextDistribution;
+    }
+
+    private static double[][] calculateMessageAndCipherDistribution(
+            double[] messageDistribution,
+            double[] keyDistribution,
+            int[][] cipherMatrix) {
+        double[][] messageAndCiphertextDistribution = new double[CIPHERTEXT_POWER][messageDistribution.length];
+        for (int i = 0; i < CIPHERTEXT_POWER; i++) {
+            messageAndCiphertextDistribution[i] = new double[messageDistribution.length];
+        }
+
+
+        for (int cipher = 0; cipher < CIPHERTEXT_POWER; cipher++) {
+            for (int message = 0; message < messageDistribution.length; message++) {
+
+                for (int key = 0; key < keyDistribution.length; key++) {
+                    if (cipherMatrix[key][message] == cipher) {
+                        messageAndCiphertextDistribution[cipher][message] += messageDistribution[message] * keyDistribution[cipher];
+                    }
+                }
+
+            }
+        }
+
+        return messageAndCiphertextDistribution;
+    }
+
 }
